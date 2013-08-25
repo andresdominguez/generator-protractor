88533fc248a268d01acba5b6bea7d3bc39b58557
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-08-25 12:21:51 -0400

    remove redundant fundtion name

--- app/index.js
+++ app/index.js
@@ -16,7 +16,7 @@ var ProtractorGenerator = module.exports = function ProtractorGenerator(args, op
 
 util.inherits(ProtractorGenerator, yeoman.generators.Base);
 
-ProtractorGenerator.prototype.askFor = function askFor() {
+ProtractorGenerator.prototype.askFor = function() {
   var cb = this.async();
 
   console.log('Welcome to the protractor code generator.\n');
