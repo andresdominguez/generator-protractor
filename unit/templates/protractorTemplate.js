753c9dcb9e50fc13e9ccd122149927118c5a885c
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-08-30 22:38:59 -0400

    fix jshint errors

--- unit/templates/protractorTemplate.js
+++ unit/templates/protractorTemplate.js
@@ -1,16 +1,16 @@
-var util = require('util');
+/*global describe, expect, protractor, it*/
+'use strict';
 
 describe('angularjs homepage', function() {
   var ptor = protractor.getInstance(),
       TIMEOUT = 10000;
 
   it('should greet using binding', function() {
-
     ptor.get('http://www.angularjs.org');
 
-    ptor.findElement(protractor.By.input("yourName")).sendKeys("Julie");
+    ptor.findElement(protractor.By.input('yourName')).sendKeys('Julie');
 
-    ptor.findElement(protractor.By.binding("Hello {{yourName}}!")).
+    ptor.findElement(protractor.By.binding('Hello {{yourName}}!')).
         getText().then(function(text) {
           expect(text).toEqual('Hello Julie!');
         });
