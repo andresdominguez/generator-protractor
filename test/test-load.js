317ea9e3ad092f5fa9f91e3a6c2bab14d7a20924
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-08-30 22:40:03 -0400

    fix jshint errors

--- test/test-load.js
+++ test/test-load.js
@@ -1,10 +1,10 @@
-/*global describe, beforeEach, it*/
+/*global describe, it, require*/
 'use strict';
 
-var assert  = require('assert');
+var assert = require('assert');
 
-describe('protractor generator', function () {
-  it('can be imported without blowing up', function () {
+describe('protractor generator', function() {
+  it('can be imported without blowing up', function() {
     var app = require('../app');
     assert(app !== undefined);
   });
