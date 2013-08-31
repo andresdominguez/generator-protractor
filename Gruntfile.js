849cc21fa53d3146e81ba51d13346e44ff77f804
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-08-30 22:37:40 -0400

    add grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'app/**/*.js',
+        'unit/**/*.js',
+        'test/*.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+    mochacli: {
+      options: {
+        bail: true
+      },
+      all: ['test/*.js']
+    },
+    watch: {
+      scripts: {
+        files: [
+          'Gruntfile.js',
+          'app/**/*.js',
+          'unit/**/*.js',
+          'test/**/*.js'
+        ],
+        tasks: ['jshint', 'mochacli'],
+        options: {
+          spawn: false
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-mocha-cli');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['mocha']);
+};
