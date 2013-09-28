cd11f2300674066a5902e79e02760097c22fd091
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-09-27 23:21:36 -0400

    Add two examples for expect.

--- unit/templates/protractorTemplate.js
+++ unit/templates/protractorTemplate.js
@@ -5,13 +5,21 @@ describe('angularjs homepage', function() {
   var ptor = protractor.getInstance();
 
   it('should greet using binding', function() {
+    // Go to the Angular website.
     ptor.get('http://www.angularjs.org');
 
+    // Find the input of the hello example and enter a value.
     ptor.findElement(protractor.By.input('yourName')).sendKeys('Julie');
 
-    ptor.findElement(protractor.By.binding('Hello {{yourName}}!')).
-        getText().then(function(text) {
-          expect(text).toEqual('Hello Julie!');
-        });
+    // Find by binding.
+    var locator = protractor.By.binding('Hello {{yourName}}!');
+
+    // The Protractor API returns promises.
+    ptor.findElement(locator).getText().then(function(text) {
+      expect(text).toEqual('Hello Julie!');
+    });
+
+    // You can also use the short version.
+    expect(ptor.findElement(locator).getText()).toEqual('Hello Julie!');
   });
 });
