e4ccfd0ffb57ffdf3890866be2e24e1ea2ac737b
Andres Dominguez <andres.dominguez.p@gmail.com>
2014-01-23 20:42:54 -0500

    Simplify file

--- app/templates/configTemplate.js
+++ app/templates/configTemplate.js
@@ -1,22 +1,12 @@
-// A reference configuration file.
 exports.config = {
   // The address of a running selenium server.
   seleniumAddress: 'http://localhost:4444/wd/hub',
 
-  // ----- What tests to run -----
-  //
   // Spec patterns are relative to the location of this config.
-  // For example: 'spec/*_spec.js',
   specs: [
     'spec/*_spec.js'
   ],
 
-  // ----- Capabilities to be passed to the webdriver instance ----
-  //
-  // For a full list of available capabilities, see
-  // https://code.google.com/p/selenium/wiki/DesiredCapabilities
-  // and
-  // https://code.google.com/p/selenium/source/browse/javascript/webdriver/capabilities.js
   capabilities: {
     'browserName': 'chrome',
     'chromeOptions': {'args': ['--disable-extensions']}
@@ -26,21 +16,11 @@ exports.config = {
   // with relative paths will be prepended with this.
   baseUrl: '<%=baseUrl%>',
 
-  // Selector for the element housing the angular app - this defaults to
-  // body, but is necessary if ng-app is on a descendant of <body>
-  rootElement: 'body',
-
-  // ----- Options to be passed to minijasminenode -----
   jasmineNodeOpts: {
-    // onComplete will be called just before the driver quits.
     onComplete: null,
-    // If true, display spec names.
     isVerbose: false,
-    // If true, print colors to the terminal.
     showColors: true,
-    // If true, include stack traces in failures.
     includeStackTrace: true,
-    // Default time to wait in ms before a test fails.
     defaultTimeoutInterval: 10000
   }
 };
