fc3521e8fef147e23de2e9c7df98ad6075efbaea
andres <code@karateca.com>
2014-03-04 22:25:04 -0500

    upgrade to new format. Start using log

--- app/index.js
+++ app/index.js
@@ -1,67 +1,66 @@
 'use strict';
-
 var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
 
 
-var ProtractorGenerator = module.exports = function ProtractorGenerator(args, options) {
-  yeoman.generators.Base.apply(this, arguments);
+var ProtractorGenerator = yeoman.generators.Base.extend({
+  init: function() {
+    this.pkg = require('../package.json');
+    var self = this;
 
-  var self = this;
-
-  this.on('end', function() {
-    this.installDependencies({
-      bower: false,
-      skipInstall: options['skip-install'],
-      callback: function() {
-        var readme = self.readFileAsString('README.txt');
-        console.log('Done! Now follow these steps:\n');
-        console.log(readme);
-        console.log('\nYou can read these instructions in README.txt\n');
-      }
+    this.on('end', function() {
+      this.installDependencies({
+        bower: false,
+        skipInstall: this.options['skip-install'],
+        callback: function() {
+          var readme = self.readFileAsString('README.txt');
+          self.log('Done! Now follow these steps:');
+          self.log(readme);
+          self.log('You can read these instructions in README.txt');
+        }
+      });
     });
-  });
+  },
 
-  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
-};
+  askFor: function() {
+    var done = this.async();
 
-util.inherits(ProtractorGenerator, yeoman.generators.Base);
+    this.log(chalk.magenta('Welcome to the protractor code generator.'));
 
-ProtractorGenerator.prototype.askForConfigName = function() {
-  var cb = this.async();
+    var prompts = [
+      {
+        type: 'input',
+        name: 'configName',
+        message: 'Choose a name for the protractor configuration file',
+        default: 'protractor-config.js'
+      },
+      {
+        type: 'input',
+        name: 'baseUrl',
+        message: 'Choose a base URL',
+        default: 'http://localhost:8000'
+      }
+    ];
 
-  console.log('Welcome to the protractor code generator.\n');
+    this.prompt(prompts, function(props) {
+      this.configName = props.configName;
+      this.baseUrl = props.baseUrl;
+      done();
+    }.bind(this));
+  },
 
-  var prompts = [
-    {
-      type: 'input',
-      name: 'configName',
-      message: 'Choose a name for the protractor configuration file',
-      default: 'protractor-config.js'
-    },
-    {
-      type: 'input',
-      name: 'baseUrl',
-      message: 'Choose a base URL',
-      default: 'http://localhost:8000'
-    }
-  ];
+  app: function() {
+    this.mkdir('spec');
+  },
 
-  this.prompt(prompts, function(props) {
-    this.configName = props.configName;
-    this.baseUrl = props.baseUrl;
-    cb();
-  }.bind(this));
-};
+  projectfiles: function() {
+    this.copy('configTemplate.js', this.configName);
+    this.copy('package.json');
+    this.copy('example_spec.js', 'spec/example_spec.js');
+    this.copy('README.txt');
+  }
+});
 
-ProtractorGenerator.prototype.app = function app() {
-  this.mkdir('spec');
-};
-
-ProtractorGenerator.prototype.createFiles = function() {
-  this.copy('configTemplate.js', this.configName);
-  this.copy('package.json');
-  this.copy('example_spec.js', 'spec/example_spec.js');
-  this.copy('README.txt');
-};
+module.exports = ProtractorGenerator;
